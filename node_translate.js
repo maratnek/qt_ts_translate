e535e379ec70e209811804df8a8c9ce44844e90f
Марат Забиров <m.zabirov@innopolis.ru>
2018-10-04 16:30:53 +0300

    change

--- node_translate.js
+++ node_translate.js
@@ -19,6 +19,29 @@ var xml = require('fs').readFileSync('GeoMehanika_ru.ts', 'utf8');
 //var options = { compact: true, ignoreComment: true, spaces: 4 };
 var result = convert.xml2js(xml);
 console.log(result);
+function transObj(result)
+{
+    for(let elem of result.elements[1].elements)
+    {
+    //console.log(elem);
+        for(let subelem of elem.elements)
+        {
+            if (subelem.name == 'name')
+                console.log(subelem);
+            else if (subelem.name == 'message') {
+                //console.log(subelem);
+                let source_text = subelem.elements[0].elements[0].text;
+                //let source_text = subelem.elements[1].elements[0].text;
+                {
+                    console.log(source_text);
+                    subelem.elements[1].elements = [];
+                    subelem.elements[1].elements.push({ type: 'text', text: source_text + 'translate' });
+
+                }
+            }
+        }
+    }
+}
 //var json = convert.xml2json(xml);
 //console.log(json);
 //var res = convert.xml2js()
