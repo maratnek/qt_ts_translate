266bc2394651dc3d49cf84dcb3546277f3acc622
Марат Забиров <m.zabirov@innopolis.ru>
2018-10-04 16:02:58 +0300

    add xml json parser

--- node_translate.js
+++ node_translate.js
@@ -1,5 +1,6 @@
 var translate = require('yandex-translate')('trnsl.1.1.20181004T120928Z.b906e018477d23d6.f087473879a9a774d61c762c5ea92e96b28c11dd');
 var fs = require('fs');
+var convert = require('xml-js');
  
 //translate.translate('Hi. How are you bro?', { to: 'ru' }, (err, res) => {
 //  console.log(res.text);
@@ -10,7 +11,16 @@ var fs = require('fs');
 //    console.log(res.lang);
 //});
 
-fs.readFile('GeoMehanika_ru.ts', (err, data) => {
-    if (err) throw err;
-    console.log(data)
-});
+//fs.readFile('GeoMehanika_ru.ts', (err, data) => {
+//    if (err) throw err;
+//    console.log(data)
+//});
+var xml = require('fs').readFileSync('GeoMehanika_ru.ts', 'utf8');
+//var options = { compact: true, ignoreComment: true, spaces: 4 };
+var result = convert.xml2js(xml);
+console.log(result);
+//var json = convert.xml2json(xml);
+//console.log(json);
+//var res = convert.xml2js()
+
+//console.log(convert);
