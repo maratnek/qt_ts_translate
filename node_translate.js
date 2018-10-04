7ef61fc3aab84e7957d62dc88a8a4719ce6fac21
Marat <marat@MacBook-Pro-Marat.local>
2018-10-05 00:04:15 +0300

    bug fix and refactoring

--- node_translate.js
+++ node_translate.js
@@ -1,11 +1,11 @@
 var translate = require('yandex-translate')('trnsl.1.1.20181004T120928Z.b906e018477d23d6.f087473879a9a774d61c762c5ea92e96b28c11dd');
 var fs = require('fs');
 var convert = require('xml-js');
- 
+
 //translate.translate('Hi. How are you bro?', { to: 'ru' }, (err, res) => {
 //  console.log(res.text);
 //});
- 
+
 //translate.detect('Граждане Российской Федерации имеют право собираться мирно без оружия, проводить собрания, митинги и демонстрации, шествия и пикетирование', function(err, res) {
 //   // res.lang -> 'ru'
 //    console.log(res.lang);
@@ -15,10 +15,34 @@ var convert = require('xml-js');
 //    if (err) throw err;
 //    console.log(data)
 //});
+
+
 var xml = require('fs').readFileSync('GeoMehanika_ru.ts', 'utf8');
 //var options = { compact: true, ignoreComment: true, spaces: 4 };
-var result = convert.xml2js(xml);
-console.log(result);
+var tsJsObj = convert.xml2js(xml);
+console.log(tsJsObj);
+
+transObj(tsJsObj);
+show(tsJsObj);
+let xmlNew = convert.js2xml(tsJsObj);
+
+fs.writeFileSync('GeoNew_ru.ts', xmlNew);
+
+async function rusTranslate(sentence) {
+  console.log(sentence);
+  let result = await (new Promise((res,rej)=>{
+    setTimeout(2000, ()=> {
+      console.log('timeout 2 second')
+      resolve('add_translate');
+    });
+  }));
+  return sentence + result;
+  // translate.translate(source_text, { to: 'ru' }, (err, res) => {
+  //   subelem.elements[1].elements.push({ type: 'text', text: res.text });
+  //   resolve(res.text);
+  // });
+}
+
 async function transObj(result)
 {
     for(let elem of result.elements[1].elements)
@@ -37,12 +61,10 @@ async function transObj(result)
                     subelem.elements[1].elements = [];
 
                     let promise = new Promise((resolve, reject)=>{
-                        translate.translate(source_text, { to: 'ru' }, (err, res) => {
-                            subelem.elements[1].elements.push({ type: 'text', text: res.text });
-                            resolve(res.text);
-                        });
+                      resolve(rusTranslate());
                     });
                     let result = await promise;
+                    subelem.elements[1].elements.push(result);
 
                 }
             }
@@ -74,15 +96,3 @@ function show(result)
         }
     }
 }
-
-transObj(result);
-show(result);
-let xmlNew = convert.js2xml(result);
-
-fs.writeFileSync('GeoNew_ru.ts', xmlNew);
-
-//var json = convert.xml2json(xml);
-//console.log(json);
-//var res = convert.xml2js()
-
-//console.log(convert);
